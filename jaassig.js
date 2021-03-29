80c5b37722bf7e5a7f52068ee6e35cfd94b6ac13
majidsawadi <majidsawadi.fusion@gmail.com>
2021-03-29 17:43:30 +0900

    week two day 1 js assignment

---
+++ jaassig.js
@@ -0,0 +1,31 @@
+
+let pi =3.14;
+console.log(typeof pi);
+let raduis;
+/* raduis */
+let raduis2 =5;
+let raduis =raduis2;
+console.log(typeof raduis);
+/* area */
+let area =raduis*pi**2;
+console.log("the area of circle givin raduis "+area);
+raduis =7;
+/* condictons */
+let x=7 ;
+console.log(x+"is positive number ");
+ let a =2;
+ let b =55;
+ let c =88;
+ console.log("the max is" +Math.max(a,b,c)  +"the min is"+Math.min(a,b,c));
+
+
+ /* for loop*/
+ var a = 10; 
+var i;
+ for(i =0; i <a; i++){
+ console.log(i);
+ }
+
+ /*math random */
+ console.log(Math.random());
+ console.log(Math.floor(Math.random() * 3));
